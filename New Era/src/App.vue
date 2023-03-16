afbbf245514c42f8e486e04f3cf4da1eda9a905b
楠ちゃん <kusu@kusu.moe>
2023-03-16 13:12:19 +0800

    Ver 0.0.0-A1:New Era.

---
+++ New Era/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import header from './components/header.vue'
+import index from './components/index.vue'
+import footer from './components/footer.vue'
+</script>
+
+<template>
+  <header />
+  <index msg="New Era" />
+  <footer />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ New Era/src/components/index.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="card">
+    <span>test</span>
+  </div>
+  <!--<div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>-->
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+h1{
+  font-size: 72px;
+  color: #000;
+}
+.card{
+  color: #fff;
+}
+</style>
